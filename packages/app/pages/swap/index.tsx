28a8064c69232803900e30616d5e3735b939fc68
Dan Lynch <pyramation@gmail.com>
2022-04-26 15:26:46 -0700

    types

--- packages/app/pages/swap/index.tsx
+++ packages/app/pages/swap/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Layout } from '@cosmology/react';
+import { Layout, SwapTokens } from '@cosmology/react';
 
 export default () => {
-  return <Layout>base wallet - all chains</Layout>;
+  return <Layout>
+    <SwapTokens></SwapTokens>
+  </Layout>;
 };
