cedf3d57c65116b7d475f58c0c2c188ea3f4d20d
JunLiu93 <853480121@qq.com>
2022-05-18 11:53:08 +0800

    change source of Layout and AssetList to added components to aviod module not found error

--- packages/app/pages/index.js
+++ packages/app/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout } from '@cosmology/react';
+import { Layout } from '../components/layout';
 import { Transactions } from '../components/mobx-testing/Transactions';
 export default () => {
   return (
--- packages/app/pages/wallet/index.tsx
+++ packages/app/pages/wallet/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Layout, AssetList } from '@cosmology/react';
+// import { Layout, AssetList } from '@cosmology/react';
+import { Layout } from '../../components/layout';
+import AssetList from '../../components/staking/AssetList';
 
 export default () => {
-  return <Layout><AssetList /></Layout>;
+  return (
+    <Layout>
+      <AssetList />
+    </Layout>
+  );
 };
